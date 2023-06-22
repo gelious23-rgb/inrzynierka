a980cefc2cd155a4ca9a58599dfb03ed04df8028
Mykhailo Baranets <baranets99934@gmail.com>
2023-06-23 01:36:53 +0200

    Fix some problems :)

--- Assets/Script/Player/Player.cs
+++ Assets/Script/Player/Player.cs
@@ -4,26 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public string Name { get; private set; }
-    public int Resources { get; private set; }
-
-    public void Initialize(string name)
-    {
-        Name = name;
-        // Set initial resources or any other necessary setup
-    }
-
-    public void StartTurn()
-    {
-        // Perform any actions specific to the start of the turn
-        // Generate resources or mana, draw cards, etc.
-    }
-
-    public void EndTurn()
-    {
-        // Perform any cleanup or end-of-turn actions
-        // Reset resources, check for end-of-turn effects, etc.
-    }
-
-
+    private int _hp;
+    private int _id;
+    
+    
+    
 }
