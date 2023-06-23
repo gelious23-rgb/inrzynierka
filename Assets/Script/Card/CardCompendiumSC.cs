e5d36551afed2a9453da705677f0c78a87e5fe0c
Gleb <tomaily.gleb@gmail.com>
2023-06-23 03:51:13 +0200

    refactor

---
+++ Assets/Script/Card/CardCompendiumSC.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardCompendiumSC : MonoBehaviour
+{
+    public Button onclick; 
+      public TextMeshProUGUI name_;
+    public TextMeshProUGUI desc, Type, hp, atk, cost;
+    [SerializeField] public Image artwork; 
+}
