89488bdbdcf61805a317c87eac1f89e04d1a26fc
nemchuk313 <rnemchuk@edu.cdv.pl>
2023-06-28 22:24:42 +0200

    player_hand_&_board_limits

--- Assets/Script/Card/CardDragDrop.cs
+++ Assets/Script/Card/CardDragDrop.cs
@@ -10,12 +10,15 @@ public class CardDragDrop : MonoBehaviour, IPointerDownHandler, IDragHandler, IE
     private GameObject _playerHand;
     private GameObject _playerBoard;
     private GameObject _gameScene;
+    private Transform _currentParent;
+    private int _boardCardLimitCount;
 
-    public void Initialize(GameObject hand, GameObject board , GameObject scene)
+    public void Initialize(GameObject hand, GameObject board , GameObject scene , int boardCardLimit)
     {
         _playerHand = hand;
         _playerBoard = board;
         _gameScene = scene;
+        _boardCardLimitCount = boardCardLimit;
         _canvas = GetComponentInParent<Canvas>();
         _rectTransform = GetComponent<RectTransform>();
         canvasGroup = GetComponent<CanvasGroup>();
@@ -23,6 +26,7 @@ public class CardDragDrop : MonoBehaviour, IPointerDownHandler, IDragHandler, IE
 
     public void OnPointerDown(PointerEventData eventData)
     {
+        _currentParent = _rectTransform.parent;
 
         _rectTransform.SetParent(_gameScene.transform);
 
@@ -43,11 +47,27 @@ public class CardDragDrop : MonoBehaviour, IPointerDownHandler, IDragHandler, IE
 
         Debug.Log("Drag ended over: " + eventData.pointerEnter);
 
-        // Check if the card is dropped on the board
-        if (eventData.pointerEnter != null && eventData.pointerEnter.transform == _playerBoard.transform)
+        // IF CARD IS DROPPED ON THE PLAYER BOARD FROM PLAYER HAND
+        if (eventData.pointerEnter != null && eventData.pointerEnter.transform == _playerBoard.transform && _currentParent.transform == _playerHand.transform)
         {
-            // Make the card a child of the board
-            _rectTransform.SetParent(_playerBoard.transform);
+            if(_playerBoard.transform.childCount < _boardCardLimitCount)
+            {
+                // Make the card a child of the board
+                _rectTransform.SetParent(_playerBoard.transform);
+            }
+            else
+            {
+                Debug.Log("Board is Full");
+                _rectTransform.position = _originalPosition;
+                _rectTransform.SetParent(_currentParent.transform);
+            }
+        }
+        else
+        //FROM PLAYER BOARD U CANT MOVE CARD
+        if (eventData.pointerEnter != null && _currentParent.transform == _playerBoard.transform)
+        {
+            _rectTransform.position = _originalPosition;
+            _rectTransform.SetParent(_currentParent.transform);
         }
         else
         {
--- Assets/Script/Game/BoardMenager.cs
+++ Assets/Script/Game/BoardMenager.cs
@@ -6,41 +6,122 @@ public class BoardMenager : MonoBehaviour
 {
 
     [SerializeField]
+    private int _playerDeckCardCount;
+    [SerializeField]
+    private int _enemyDeckCardCount;
+    [SerializeField]
+    private int _playerHandCardLimitCount;
+    [SerializeField]
+    private int _enemyHandCardLimitCount;
+    [SerializeField]
+    private int _boardCardLimitCount;
+
+    [SerializeField]
     private GameObject _canvas;
+
     [SerializeField]
     private GameObject _playerHand;
     [SerializeField]
     private GameObject _playerBoard;
     [SerializeField]
+    private GameObject _enemyHand;
+    [SerializeField]
+    private GameObject _enemyBoard;
+
+
+    [SerializeField]
+    private Transform _cardInstanceTransform;
+    [SerializeField]
     private GameObject _cardPrefab;
     [SerializeField]
     private List<Card> _cards = new List<Card>();
+    [SerializeField]
+    private List<GameObject> _playerDeck;
+    [SerializeField]
+    private List<GameObject> _enemyDeck;
 
     void Start()
     {
-        
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            InitializePlayerDeck();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        AddCardToHand();
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            InitializePlayerDeck();
+            InitializeEnemyDeck();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            AddCardToPlayerHand(3);
+        }
     }
 
-    private void AddCardToHand()
+    private void AddCardToPlayerHand(int p_count)
     {
-        if(Input.GetKeyDown(KeyCode.G))
+        if (_playerHand.transform.childCount + p_count > _playerHandCardLimitCount)
         {
-            GameObject cardInstance = Instantiate(_cardPrefab,_playerHand.transform,true);
+            for(int i = _playerHand.transform.childCount; i < _playerHandCardLimitCount; i ++ )
+            {
+                GameObject card = _playerDeck[0];
 
-            cardInstance.GetComponent<CardDisplay>()._card = _cards[Random.Range(0, _cards.Count)];
+                _playerDeck.Remove(_playerDeck[0]);
 
-            cardInstance.GetComponent<Transform>().transform.localScale = new Vector3(1,1,1);
+                //card.GetComponent<Transform>().transform.localScale = new Vector3(1, 1, 1);
 
-            cardInstance.transform.SetParent(_playerHand.transform);
-
-            CardDragDrop cardDragDrop = cardInstance.AddComponent<CardDragDrop>();
-            cardDragDrop.Initialize(_playerHand, _playerBoard , _canvas);
+                card.transform.SetParent(_playerHand.transform);
+            }
         }
+        else
+        {
+            for (int i = 0; i < p_count; i++)
+            {
+
+                GameObject card = _playerDeck[0];
+
+                _playerDeck.Remove(_playerDeck[0]);
+
+                //card.GetComponent<Transform>().transform.localScale = new Vector3(1, 1, 1);
+
+                card.transform.SetParent(_playerHand.transform);
+
+            }
+        }
+
+    }
+
+    private void InitializePlayerDeck()
+    {
+        for (int i = 0; i < _playerDeckCardCount; i++ )
+        {
+            _playerDeck.Add(CreateRandomCard());
+        }
+    }
+
+    private void InitializeEnemyDeck()
+    {
+        for (int i = 0; i < _enemyDeckCardCount; i++)
+        {
+            _enemyDeck.Add(CreateRandomCard());
+        }
+    }
+
+    private GameObject CreateRandomCard()
+    {
+        // Instantiate blank card with CardDisplay
+        GameObject cardInstance = Instantiate(_cardPrefab, _cardInstanceTransform);
+        //Assign scriptableObject (random cardinfo)
+        cardInstance.GetComponent<CardDisplay>()._card = _cards[Random.Range(0, _cards.Count)];
+
+        CardDragDrop cardDragDrop = cardInstance.AddComponent<CardDragDrop>();
+
+        cardDragDrop.Initialize(_playerHand, _playerBoard, _canvas,_boardCardLimitCount);
+
+        return cardInstance;
     }
 }
